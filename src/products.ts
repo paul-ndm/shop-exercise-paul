7cf2e83240e921a0fc15a85f4670e52aafcb9797
Barnabas Jovanovics <barnabas.jovnaovics@gmail.com>
2021-04-06 11:28:54 +0200

    add fake products

---
+++ src/products.ts
@@ -0,0 +1,29 @@
+const products = [
+  {
+    "picture": "http://placehold.it/100x100",
+    "price": 180,
+    "name": "cillum"
+  },
+  {
+    "picture": "http://placehold.it/100x100",
+    "price": 289,
+    "name": "ad"
+  },
+  {
+    "picture": "http://placehold.it/100x100",
+    "price": 170,
+    "name": "culpa"
+  },
+  {
+    "picture": "http://placehold.it/100x100",
+    "price": 181,
+    "name": "ut"
+  },
+  {
+    "picture": "http://placehold.it/100x100",
+    "price": 178,
+    "name": "consectetur"
+  }
+]
+
+export default products
